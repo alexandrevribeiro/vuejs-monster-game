ca7acbfd5cc962e3ddecee7c803ad0c46f454bd7
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-26 20:47:24 -0300

    Vue instanece

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import * as runner from 'express-app-runner';
 
 runner.routeHomepageToFile('./index.html');
 runner.addStaticDir('./css', '/css');
+runner.addStaticDir('./js', '/js');
 runner.run();
